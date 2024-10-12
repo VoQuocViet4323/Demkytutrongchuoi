0ff5dae34a6b994730443df2a3a1130fda420e17
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-12 22:57:01 +0700

    a

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static int CountCharacterOccurrences(string str, char character)
+    {
+        int count = 0;
+
+        foreach (char c in str)
+        {
+            if (c == character)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Nhập chuỗi: ");
+        string inputString = Console.ReadLine();
+
+        Console.Write("Nhập ký tự cần đếm: ");
+        char characterToCount = Console.ReadLine()[0];
+
+        int count = CountCharacterOccurrences(inputString, characterToCount);
+
+        Console.WriteLine($"Ký tự '{characterToCount}' xuất hiện {count} lần trong chuỗi.");
+    }
+}
